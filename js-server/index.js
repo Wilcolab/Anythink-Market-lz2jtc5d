d3f90f34474a7a2e291fc533a6c3b0052b7bb9c2
Saransh choudhary <saransh20singh@gmail.com>
2025-07-09 17:41:09 +0000

    Fix: listen on 0.0.0.0 in Node.js server for Docker compatibility

--- js-server/index.js
+++ js-server/index.js
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
   res.send('Hello from Node.js Express server!');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server listening on port ${PORT}`);
 });
